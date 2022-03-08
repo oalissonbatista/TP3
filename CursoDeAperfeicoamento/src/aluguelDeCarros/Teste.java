90b685877b78f18545e4fd21ef204a03d7d44150
alisson252525@hotmail.com <alisson252525@hotmail.com>
2022-03-08 17:55:17 -0300

    Atualização da Classe Teste

--- CursoDeAperfeicoamento/src/aluguelDeCarros/Teste.java
+++ CursoDeAperfeicoamento/src/aluguelDeCarros/Teste.java
@@ -16,31 +16,24 @@ public class Teste {
 		
 		Reserva aux;
 		
-		GregorianCalendar d= new GregorianCalendar();
-		Date agora = d.getTime();
+		Carro carro = new Carro("fiat Mobi", "fiat", "Econômico", "Eletrico", 530.76);
 		
-		Carro car1 = new Carro("fiat Mobi", "fiat", "Econômico", "Eletrico", 111.76);
-		Carro car2 = new Carro("Honda Civic", "Honda", "intermediario", "Automático", 200.20);
-		
-	    Telefone tf = new Telefone(61, 99999999);
-		f = new Funcionario("Ronaldo", agora, 777777, tf, "Brasilia", 1000.00 , 77777);
+		Telefone telefoneFuncionario = new Telefone(61, 998378898);
+		f = new Funcionario("Ronaldo","18/05/1970", 777777777, telefoneFuncionario, "Brasilia", 1000.00 , 77777);
 		
 		cup = new Cupom("ASDDFSE", "10% de desconto");
 		
 		cart = new Carteira("pix", "cartao", "pix", "valePresente");
 		
-		Telefone tc = new Telefone(61, 993031494);
+		Telefone telefoneCliente = new Telefone(38, 993031494);
+		c = new Cliente("Alisson","15/02/1981", 733098421, telefoneCliente, 
+				         "Brasilia", "alisson252525@hotmail.com", "senha", cart, 3812167);
 		
-		c = new Cliente("Alisson", agora, 733098421, tc, "Brasilia", "alisson252525@hotmail.com", "senha", cart, 3812167);
-		
-		aux = new Reserva("Aeroporto de Brasilia", agora, "Aeroporto de Brasilia", agora, 111.76, cup, f, c, car1);
+		aux = new Reserva("Aeroporto de Brasilia", "21/03/2022 10:00", "Aeroporto de Brasilia", 
+				          "27/03/2022 10:00", 700.00, cup, f, c, carro);
 		aux.reservar();
 		
 		System.out.println(aux.toString());
-		System.out.println(cup.toString());
-		System.out.println(c.toString());
-		System.out.println(f.toString());
-	
 	}
 
 }
